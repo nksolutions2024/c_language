bfa77aa00fb160cb84fbc4a4d8f96e2bdf692f6c
desd <sandesh4080@gmail.com>
2025-09-22 23:52:03 +0530

    needed to malloc to each structure(something)

---
+++ a8/structpointer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct player_info{
+	int id;
+	int age;
+};
+
+int main(){
+	
+	struct player_info *p[100];
+
+	void* ptr;
+	ptr = malloc(sizeof(struct player_info)); // ptr gets 8bytes
+
+	struct player_info* s_ptr; //pointer to structure
+
+	s_ptr = (struct player_info *)ptr; //typecasting
+////////////////////////////////////////////////////////////////////
+	p[0] = (struct player_info *)malloc(sizeof(struct player_info));
+
+	scanf("%d",&p[0]->id);
+
+	printf("\n %d \n", p[0]->id);
+
+
+	return 0;
+}
