6995fd9f873f538332e2e4039f4f5a93ed6eed1b
desd <sandesh4080@gmail.com>
2025-09-22 23:22:48 +0530

    q9 2322

--- a8/q9.c
+++ a8/q9.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int no_of_students=0;
 
@@ -18,20 +19,21 @@ int main(){
 	struct student_data *blue[100];
 	
 	int yes=1;
-	while(yes != 0)
+//	while(yes == 0)
+	for(i=0; i<4; i++)
 	{
 		printf("Enter value of YES(1 or 0) : ");
-		scanf("%d", &yes);
+//		scanf("%d", &yes);
 
 		void *ptr;
 		ptr = malloc(sizeof(struct student_data));
-		struct student_rec * s_ptr;
+		struct student_data * s_ptr;
 		s_ptr = (struct student_data *)ptr;
 		
 		printf("\n Enter name of student-%d: ", no_of_students+1);
-		scanf("%s", s_ptr.name);
-		printdf("\n Enter age of student-%d: ", no_of_students+1);
-		scanf("%d", s_ptr.age);
+		scanf("%s", s_ptr->name);
+		printf("\n Enter age of student-%d: ", no_of_students+1);
+		scanf("%d", &s_ptr->age);
 
 		blue[no_of_students++] = s_ptr;
 	}
@@ -39,8 +41,8 @@ int main(){
 	//try function in next phase			
 	for(i=0; i<1; i++)
 	{
-		printf("\n %s \n", blue[i].name);
-		printf("\n %d \n", blue[i].age);
+		printf("\n %s \n", blue[i]->name);
+		printf("\n %d \n", blue[i]->age);
 	}
 
 	return 0;
