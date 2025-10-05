0de8878fc965ef6664a229efcc5edba6a40d98d8
kite <sandesh4080@gmail.com>
2025-10-05 20:57:21 +0530

    2 syntax issue

--- dsa/recbs.c
+++ dsa/recbs.c
@@ -3,7 +3,7 @@
 int rbinSearch(int arr[], int start, int end, int x){
 	if(end >= start)
 	{
-		if mid = start + (end-start)/2;
+		int mid = start + (end-start)/2;
 		if(arr[mid]==x)
 		{
 			return mid;
@@ -22,6 +22,6 @@ int rbinSearch(int arr[], int start, int end, int x){
 }
 
 int main(){
-	arr[]={10,20,30};
+	int arr[]={10,20,30};
 	printf("\n%d\n", rbinSearch(arr,10,30,20) );
 }
