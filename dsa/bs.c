66f74347f029eaf83b95b31f48dfbe1690dbdbce
kite <sandesh4080@gmail.com>
2025-10-05 20:49:21 +0530

    binary search code pending

--- dsa/bs.c
+++ dsa/bs.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+
+int binSearch(int* p, int start, int end, int x);
+
+int main(){
+	arr[]={10,20,30};
+	binSearch(arr, , ,20); //issue
+
+
+}
+
+int binSearch(int *p, int start, int end, int x){
+	while(start <= end)
+	{
+		int mid = start + (end-start)/2;
+		if(p[mid]==x)
+			return mid;
+		if(p[mid]<x)
+			start=mid+1;
+		else
+			printf("--");
+	}
+}
 
