aa9a6de4e28f3e284c56712701e38260006fe066
desd <sandesh4080@gmail.com>
2025-10-06 09:30:19 +0530

    q1 g trying

--- dsa/a1/q1/q1.c
+++ dsa/a1/q1/q1.c
@@ -5,15 +5,18 @@ int arr[5]= {-99,-99,-99,-99,-99};
 #include "myheader.h" //include the header file
 
 void revDisplay();
+int search(int element);
 		      
 int main(){
-	int u_index,u_value;
+	int u_index, u_value, pos ;
 	int choice;
+
+//	arr[]={10,20,30,40,50};
 
 	while(1)
 	{
 		printf("Enter 1->insert 2->display 3->exit 4->delByIndex ");
-	      	printf("5->deleteByEle 8->reverse_display : ");
+	      	printf("5->deleteByEle 8->reverse_display 9->search : ");
 		scanf("%d",&choice);
 		switch(choice)
 		{
@@ -30,19 +33,27 @@ int main(){
 				break;
 
 			case 4:
-				printf("Enter index for deleting");
+				printf("Enter index for deleting: ");
 				scanf("%d", &u_index);
 				deleteByIndex(u_index);
 				break;
 
 			case 5:
-				printf("Enter value to be deleted");
+				printf("Enter value to be deleted: ");
 				scanf("%d",&u_value);
 				deleteByEle(u_value);
 				break;
 
 			case 8:
 				revDisplay();
+				break;
+
+			case 9:
+				printf("Enter element to be searched: ");
+				scanf("%d", &u_value);
+				pos = search(u_value);
+				printf("Element at index:%d",pos);
+				//case2 not found ; pos = -1
 				break;
 
 			case 3:
@@ -57,4 +68,15 @@ void revDisplay(){
 	{
 		printf("%d \t", arr[i]);
 	}
+	printf("\n");
 }
+
+int search(int element){
+	for(int i=0; i<5; i++)
+	{
+		if(arr[i]==element)
+			return i;
+	}
+	return -1;
+}
+
