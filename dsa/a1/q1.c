7046e5d007fd0aa650a7b50780cc9ab01e341a49
desd <sandesh4080@gmail.com>
2025-10-06 07:46:20 +0530

    delete 2 ways

--- dsa/a1/q1.c
+++ dsa/a1/q1.c
@@ -5,6 +5,8 @@ int arr[5]= {-99,-99,-99,-99,-99};
 
 void insertAtIndex(int n,int index);
 void display();
+void deleteAtIndex(int index);
+void deleteByEle(int ele);
 
 int main(){
 	int u_index,u_value;
@@ -12,7 +14,7 @@ int main(){
 
 	while(1)
 	{
-		printf("Enter the 1->insert 2->display 3->exit : ");
+		printf("Enter the 1->insert 2->display 3->exit 4->deleteByIndex 5->deleteByEle : ");
 		scanf("%d",&choice);
 		switch(choice)
 		{
@@ -25,6 +27,16 @@ int main(){
 				break;
 			case 2: 
 				display();
+				break;
+			case 4:
+				printf("Enter index for deleting");
+				scanf("%d", u_index);
+				deleteByIndex(u_index);
+				break;
+			case 5:
+				printf("Enter value to be deleted");
+				scanf("%d",u_value);
+				deleteByEle(u_value);
 				break;
 			case 3:
 				exit(0);
@@ -50,5 +62,29 @@ void display(){
 	for(i=0; i<5; i++)
 	{
 		printf("%d\n",arr[i]);
+	}
+}
+
+void deleteAtIndex(int index){
+	if (arr[index] != -99)
+	{
+		printf("Deleted element was %d\n",arr[index]);
+		arr[index] = -99;
+	}
+	else
+	{
+		printf("Already empty\n");
+	}
+
+}
+
+void deleteAtEle(int ele)
+{
+	for(int i=0; i<n; i++)
+	{
+		if(arr[i] == ele)
+		{
+			arr[i]=-99;
+		}
 	}
 }
