9bee3e8b28cc52b37a8199151b84bb88f29c0fed
desd <sandesh4080@gmail.com>
2025-10-07 21:14:26 +0530

    warning but working

--- dsa/a1/q2.c
+++ dsa/a1/q2.c
@@ -2,6 +2,8 @@
 
 struct emp_data{
 	int empid;
+	char empName[20];
+	int salary;
 	int yearOfJoin;
 }emp1,emp2;
 
@@ -9,8 +11,22 @@ int main(){
 	emp1.empid = 101;
 	emp1.yearOfJoin = 2008;
 
+	printf("enter the name : ");
+	scanf("%s", &emp1.empName);
+
 	printf("%d\n", emp1.empid);
 	printf("%d\n", emp1.yearOfJoin);
+	printf("%s\n", emp1.empName);
    	
 
 }
+
+$ gcc q2.c
+q2.c: In function ‘main’:
+q2.c:15:17: warning: format ‘%s’ expects argument of type ‘char *’, but argument 2 has type ‘char (*)[20]’ [-Wformat=]
+   15 |         scanf("%s", &emp1.empName);
+      |                ~^   ~~~~~~~~~~~~~
+      |                 |   |
+      |                 |   char (*)[20]
+      |                 char *
+
