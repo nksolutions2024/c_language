71d66c57e0fd278110b95c84913639721c494830
desd <sandesh4080@gmail.com>
2025-10-07 21:44:38 +0530

    converted to array of struct

--- dsa/a1/q2.c
+++ dsa/a1/q2.c
@@ -6,21 +6,29 @@ struct emp_data{
 	char empName[20];
 	int salary;
 	int yearOfJoin;
-}emp1,emp2;
+}arr_struct[10];
 
 int main(){
-	emp1.empid = 101;
-	strcpy(emp1.empName,"strcopy string");
-	emp1.salary = 40000;
-	emp1.yearOfJoin = 2008;
+	arr_struct[0].empid = 101;
+	strcpy(arr_struct[0].empName,"tom daves");
+	arr_struct[0].salary = 40000;
+	arr_struct[0].yearOfJoin = 2008;
+
+	arr_struct[1].empid = 102;
+	strcpy(arr_struct[1].empName,"jake");
+	arr_struct[1].salary = 40000;
+	arr_struct[1].yearOfJoin = 2020;
 
 //	printf("enter the name : ");
 //	scanf("%s", emp1.empName);
 
-	printf("%d\n", emp1.empid);
-	printf("%s\n", emp1.empName);
-	printf("%d\n", emp1.salary);
-	printf("%d\n", emp1.yearOfJoin);
+	for(int i=0; i<2; i++)
+	{
+		printf("%d\n", arr_struct[i].empid);
+		printf("%s\n", arr_struct[i].empName);
+		printf("%d\n", arr_struct[i].salary);
+		printf("%d\n", arr_struct[i].yearOfJoin);
+	}
    	
 
 }
