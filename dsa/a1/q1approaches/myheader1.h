f2001935b6d7a008cbbc982429bb03289c0e54de
desd <sandesh4080@gmail.com>
2025-10-07 07:30:11 +0530

    2nd approach insert at index

---
+++ dsa/a1/q1approaches/myheader1.h
@@ -0,0 +1,81 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+void insertAtIndex(int n,int index){
+	// index above 4 ,warning
+	//
+	if(arr[index] == -99)
+	{
+		arr[index]=n;
+	}
+	else
+	{
+		printf("already element there\n");
+	}
+}
+
+void display(){
+	int i;
+	for(i=0; i<5; i++)
+	{
+		printf("%d\n",arr[i]);
+	}
+}
+
+void deleteByIndex(int index){
+	if (arr[index] != -99)
+	{
+		printf("Deleted element was %d\n",arr[index]);
+		arr[index] = -99;
+	}
+	else
+	{
+		printf("Already empty\n");
+	}
+
+}
+
+void deleteByEle(int ele)
+{
+	for(int i=0; i<5; i++)
+	{
+		if(arr[i] == ele)
+		{
+			arr[i]=-99;
+		}
+		//consider case of not found
+	}
+}
+
+int arrMin(){
+	//assume arr[0] is Min 
+	//assuming arr[0] is not -99
+	int minIndex = 0;
+	for(int i; i<5; i++)
+	{
+		if(arr[i]<arr[minIndex] && arr[i]!=-99)
+		{
+			minIndex=i;
+		}
+	}
+	return minIndex;
+}
+
+void revDisplay(){
+	for(int i=4; i>=0; i--)
+	{
+		printf("%d \t", arr[i]);
+	}
+	printf("\n");
+}
+
+int search(int element){
+	for(int i=0; i<5; i++)
+	{
+		if(arr[i]==element)
+			return i;
+	}
+	return -1;
+}
+
+#endif
---
+++ dsa/a1/q1approaches/q1insert2ndapproach.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int arr[5]={-99,-99,-99,-99,-99};
+#include "myheader1.h" //include the header file
+
+void i_approach2nd(int index, int value);
+		      
+int main(){
+	int u_index, u_value ;
+	int choice;
+
+	while(1)
+	{
+		printf("Enter 1->insert 2->display 3->exit ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				printf("Enter index where to insert : ");
+				scanf("%d", &u_index);
+				printf("Enter value of element : ");
+				scanf("%d", &u_value);
+				insertAtIndex(u_value, u_index);
+				break;
+
+			case 4:
+				printf("Enter index where to insert : ");
+				scanf("%d", &u_index);
+				printf("Enter value of element : ");
+				scanf("%d", &u_value);
+				i_approach2nd(u_index, u_value);
+				break;
+
+			case 2: 
+				display();
+				break;
+
+
+			case 3:
+				exit(0);
+		}
+	}
+
+}
+
+void i_approach2nd(int index , int value ){
+	if(arr[index]==-99)
+	{
+		arr[index]=value;
+	}
+	else
+	{
+		//swift to right
+		if(arr[4]==-99)// last slot is empty
+		{
+			//half part free slot ckecking
+			for(int i=4; i>index; i--)
+			{
+				//element[3] copied to element[4]
+				arr[i] = arr[i-1];
+			}
+
+			//finally add
+			arr[index &&&&&&&&&&&&]
+	}
+}
