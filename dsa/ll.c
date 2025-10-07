171ce5f241d80b7924cc480cc0a7e3b48046e413
kite <sandesh4080@gmail.com>
2025-10-07 08:09:19 +0530

    ll 2things okk

--- dsa/ll.c
+++ dsa/ll.c
@@ -10,15 +10,17 @@ struct node* head = NULL;
 
 void insert(int);
 void display();
+void insertAtBeg(int ele);
+void insertAtPos(int ele, int pos);
 
 int main(){
-	int ele;
+	int ele,pos;
 	int choice;
 
 	while(1)
 	{
 
-	printf("enter 1->insert 2->display 3->exit \n");
+	printf("enter 1->insert 2->display 3->exit 4->insertAtBeg 5->Pos \n");
 	scanf("%d", &choice);
 
 	switch(choice){
@@ -28,6 +30,17 @@ int main(){
 			break;
 		case 2: printf("linklist \n");
 			display();
+			break;
+		case 4:
+			printf("enter ele (begin) ");
+			scanf("%d",&ele);
+			insertAtBeg(ele);
+			break;
+
+		case 5:
+			printf("enter ele and pos ");
+			scanf("%d %d",&ele,&pos);
+			insertAtPos(ele,pos);
 			break;
 		case 3: 
 			exit(0);
@@ -72,5 +85,40 @@ void display(){
 		t1=t1->next;
 	}
 	printf("\n");
+}
+
+void insertAtBeg(int ele){
+	struct node* temp;
+	void* v_ptr = malloc(sizeof(struct node));
+	temp = (struct node*)v_ptr;
+
+	temp->data = ele;
+	temp->next = NULL;
+
+	//sequence imp
+	temp->next=head;
+	head=temp;
+}
+
+void insertAtPos(int ele, int pos){
+	struct node *t1;
+	t1=head;
+
+	struct node* temp;
+	void* v_ptr = malloc(sizeof(struct node));
+	temp = (struct node*)v_ptr;
+
+	temp->data = ele;
+	temp->next = NULL;
+	
+	int i=1;
+	while(++i<pos)
+	{
+		t1=t1->next;
+	}
+
+	//A then B
+	temp->next=t1->next;
+	t1->next=temp;
 }
 
