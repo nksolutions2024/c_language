b6d5c69b6cb773da6694568e1ad5378262aecc0e
desd <sandesh4080@gmail.com>
2025-10-11 00:35:02 +0530

    display,swap functions wrritten ; bubbleSort() in_progress

--- dsa/third_a/q1.c
+++ dsa/third_a/q1.c
@@ -1,15 +1,38 @@
 #include<stdio.h>
 
-int arr[5]={10,20,40,50,30};
+int arr[6]={10,20,40,50,30};
 void display();
+void bubbleSort();
+void swap(int*,int*);
 
 int main(){
 	//bubble sort
-
+	printf("before\n");
+	display();
+	printf("after\n");
 	display();
 
 }
 
+void swap(int* a; int* b){
+	int temp;
+
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+void bubbleSort(){
+	for(int i=0; i<6; i++){
+		for(int j=1;j<6;j++){
+			if(arr[i]>arr[i+1])
+			{
+				swap(&arr[i],&arr[i+1]);
+			}
+		}
+	}
+
+}
 void display(){
 	for(int i=0; i<5; i++)
 	{
