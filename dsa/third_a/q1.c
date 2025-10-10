1c01ebb69385084d150e5a9d642c69237d91690e
desd <sandesh4080@gmail.com>
2025-10-11 00:43:04 +0530

    there was no issue in swap()

--- dsa/third_a/q1.c
+++ dsa/third_a/q1.c
@@ -6,10 +6,15 @@ void bubbleSort();
 void swap(int*,int*);
 
 int main(){
+	int var1=10,var2=20;
 	//bubble sort
 	printf("before\n");
+	printf("%d %d\n",var1,var2);
 	display();
+	
 	printf("after\n");
+	swap(&var1,&var2);
+	printf("%d %d\n",var1,var2);
 	display();
 
 }
@@ -24,7 +29,7 @@ void swap(int* a, int* b){
 
 void bubbleSort(){
 	for(int i=0; i<6; i++){
-		for(int j=i+1;j<6;j++){
+		for(int j=i+1;j<6-i-1;j++){
 			if(arr[i]>arr[i+1])
 			{
 				swap(&arr[i],&arr[i+1]);
