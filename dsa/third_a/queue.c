08042d0d195217391eb48436abb1ad5d47372a7e
desd <sandesh4080@gmail.com>
2025-10-14 19:29:08 +0530

    -99 printing okk

--- dsa/third_a/queue.c
+++ dsa/third_a/queue.c
@@ -9,7 +9,9 @@ void enqueue(int* arr, int*front, int*rear, int ele);
 
 
 int main(){
-	int choice;
+	int arr[SIZE], arr1[5]={2,2,2,2,2};
+	int choice,ele;
+	arr_init(arr);
 
 	while(1)
 	{
@@ -18,12 +20,14 @@ int main(){
 		switch(choice)
 		{
 			case 1:
-				printf("insert function pending\n");
+				printf("enter ele");
+				scanf("%d",&ele);
+//				enqueue(arr);
 				break;
 
 			case 2:
 				printf("queue is \n");
-				display();
+				display(arr);
 				break;
 
 			case 3:
@@ -35,18 +39,33 @@ int main(){
 	}
 }
 
-void arr_init(int* arr){
+void enqueue(int* a,int* f,int* r,int ele){
+	if(*r==SIZE-1)
+	{
+		printf("queue is full\n");
+		return;
+	}
+	(*r)++;
+	a[*r]=ele;
+	if(*f==-1)
+	{
+		*f=0;
+	}
+
+}
+
+void arr_init(int* arrvar){
 	for(int i=0; i<SIZE; i++)
 	{
-		arr[i]=-99;
+		arrvar[i]=-99;
 	}
 }
 
-void display(int* arr){
+void display(int* a){
 	for(int i=0; i<SIZE; i++)
 	{
-		printf("[%d]-", arr[i]);
+		printf("[%d]-", a[i]);
 	}
-	printf("\b\b||||||||\n");
+	printf("\b||||||||\n");
 }
 
