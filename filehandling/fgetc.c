9fe07f5c8072ab6e942a1e223428786b304d7ff8
kite <sandesh4080@gmail.com>
2025-11-09 19:22:20 +0530

    prog 2

--- filehandling/fgetc.c
+++ filehandling/fgetc.c
@@ -4,6 +4,7 @@ int main(){
 	//declaring FILE pointer
 	FILE *fp;
 	char ch;//datatype char; 1 byte
+	int nol=0, not=0, nob=0, noc=0;
 	//fopen function with 2 arguments
 	fp = fopen("fgetc.c","r");
 	while(1)
@@ -20,8 +21,17 @@ int main(){
 		}
 		//whatif EOF goes inside printf
 		printf("%c",ch);
+		//program-2
+		noc++;
+		if(ch==' ')
+			nob++;
+		if(ch=='\n')
+			nol++;
+		if(ch=='\t')
+			not++;
 	}
 	printf("\n---------------\n");
+	printf("%d %d %d %d",noc, nob, nol, not);
 	
 	fclose(fp); //close file pointer
 }
