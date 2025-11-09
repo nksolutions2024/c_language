31b04d157e4fa43adec2902240972eb4da94f079
kite <sandesh4080@gmail.com>
2025-11-09 19:09:59 +0530

    comments only added

--- filehandling/fgetc.c
+++ filehandling/fgetc.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 
 int main(){
+	//declaring FILE pointer
 	FILE *fp;
-	char ch;
+	char ch;//datatype char; 1 byte
+	//fopen function with 2 arguments
 	fp = fopen("fgetc.c","r");
-
 	while(1)
 	{
+		//storing 1-byte in variable "ch"
+		//fgetc func takes file-pointer
+		//fgetc reads 1 character at a time
+		//somehow increment in next-iteration
 		ch = fgetc(fp);
+		//EOF is important
 		if(ch==EOF)
 		{
 			break;
 		}
+		//whatif EOF goes inside printf
 		printf("%c",ch);
 	}
-	printf("\n");
+	printf("\n---------------\n");
 	
-	fclose(fp);
+	fclose(fp); //close file pointer
 }
