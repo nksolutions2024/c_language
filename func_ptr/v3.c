5940cf71df16b7a750b28b62b0c83cc9a4adcc71
kite <sandesh4080@gmail.com>
2025-12-27 15:35:40 +0530

    calculator special

--- func_ptr/v3.c
+++ func_ptr/v3.c
@@ -2,11 +2,23 @@
 
 float add(float, float);
 
+struct operations{
+	float (*addition)(float, float);
+	float (*subtraction)(float, float);
+};
+
 int main(){
 	float k=2.4;
-	float j=4.8;
+	float j=8.8;
 
-	float result = add(k,j);
+	struct operations var_of_struct;
+
+	var_of_struct.addition = add;
+
+	float result;
+	resullt	= var_of_struct.addition(k,j);
+
+//	float result = add(k,j);
 
 	printf("%f\n", result);
 
