e11bf9afbdb357b5e49087a657e15291ee759a7a
kite <sandesh4080@gmail.com>
2025-12-27 15:12:51 +0530

    struct of function pointers okk

--- func_ptr/v1.c
+++ func_ptr/v1.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
 
-void say_hello(){
-	printf("hhello1\n");
+int say_hello(){
+	printf("hhello void void\n");
+	return 8;
 }
 
 void say_bye(){
 	printf("ggoodbye\n");
 }
 
-int main(){
-	void (*func[2])() = {say_hello, say_bye};
+struct Actions {
+	int (*actionA)(void);
+	void (*actionB)(void);
+};
 
-	func[0]();
+int main(){
+//	void (*func[2])() = {say_hello, say_bye};
+	struct Actions var_of_struct;
+	var_of_struct.actionA = say_hello;
+	var_of_struct.actionB = say_bye;
+
+	var_of_struct.actionA();
+
+//	func[0]();
 //	say_hello();
 
-	func[1]();
+//	func[1]();
 //	say_bye();
 }
