a492c7cb58e67eaacb8feedd8c5d11d3b8dd8298
kite <sandesh4080@gmail.com>
2026-01-03 17:22:45 +0530

    pipe not properly working

---
+++ os/programssss/pipe.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main(){
+	char write_msg[BUFFER_SIZE] = "Greetings";
+	char read_msg[BUFFER_SIZE] ;
+	int fd[2];
+	pid_t pid;
+
+	if(pipe(fd) == -1)
+	{
+		fprintf(stderr, "Pipe failed");
+		return 1;
+	}
+
+	pid = fork();
+
+	if(pid < 0)
+	{
+		fprintf(stderr, "Fork failed");
+		return 1;
+	}
+
+
+	if(pid > 0)
+	{
+		//parent process
+		close(fd[READ_END]);
+		write(fd[READ_END], write_msg, strlen(write_msg) + 1);
+		close(fd[WRITE_END]);
+	}
+	else
+	{
+		//child process
+		close(fd[WRITE_END]);
+		read(fd[READ_END], read_msg, BUFFER_SIZE);
+		printf("read %s\n", read_msg);
+		close(fd[READ_END]);
+	}
+	return 0;
+}
