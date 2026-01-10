1992ca3e04d2f6da494655019221874df4218d39
kite <sandesh4080@gmail.com>
2026-01-10 17:10:35 +0530

    k, l will be incrementing

--- post_increment/k1.c
+++ post_increment/k1.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
 void main(){
-	int k,l;
+	int k,l,g,h;
 	k = 9;
 	l = 9;
 
-	printf("case a: %d\n", (k++) );
-	printf("case b: %d\n", (++l) );
+	g = k++;
+	printf("case a: %d\n", g );
+	h = ++l;
+	printf("case b: %d\n", h );
+
+	printf("case 45: %d\n", g );
+	printf("case 78: %d\n", h );
 
 }
